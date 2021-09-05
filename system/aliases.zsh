c0ec3f21818e2a54c5fe3a9573e0c0b3aa2749b7
Gabriele Bellomia <56808633+bellomia@users.noreply.github.com>
2021-09-05 11:10:22 +0200

    New system aliases: the colorful skip and clc
    🌈 🌈 🌈

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,14 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+
+# RAINBOW SKYLINES via lolcat and spark
+#   `brew install lolcat`   [Ruby version of LOLCAT]
+#   `pip install lolcat`    [Python version of LOLCAT] 
+#   `snap install lolcat-c` [C version of LOLCAT]
+#   `brew install spark` [or just append spark.sh to PATH]
+if $(lolcat -v &> /dev/null) && $(sparks &> /dev/null)
+then 
+  alias skip='echo; echo; seq 1 $(tput cols) | sort -R | spark | lolcat -a; echo; echo'
+  alias clc='clear; skip'
+fi
