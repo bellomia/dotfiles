64bf60d2ec5d631af604a89f823ea74b60604930
Gabriele Bellomia <56808633+bellomia@users.noreply.github.com>
2021-09-06 22:17:42 +0200

    Update aliases.zsh
    - Dropped lolcat-c (too different from original lolcat)
    - Changed <lolcat -v> in <lolcat -h> for compatibility with py-lolcat

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -12,9 +12,8 @@ fi
 # RAINBOW SKYLINES via lolcat and spark
 #   `brew install lolcat`   [Ruby version of LOLCAT]
 #   `pip install lolcat`    [Python version of LOLCAT] 
-#   `snap install lolcat-c` [C version of LOLCAT]
 #   `brew install spark` [or just append spark.sh to PATH]
-if $(lolcat -v &> /dev/null) && $(sparks &> /dev/null)
+if $(lolcat -h &> /dev/null) && $(spark &> /dev/null)
 then 
   alias skip='echo; echo; seq 1 $(tput cols) | sort -R | spark | lolcat -a; echo; echo'
   alias clc='clear; skip'
